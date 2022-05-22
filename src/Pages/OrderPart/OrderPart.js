df0165b51b6cbad42d6cb861fbb542bcf0d934e1
H_A _Opu <wwwhasib4@gmail.com>
2022-05-23 01:45:32 +0600

    order form is requireing with react-hook-form

--- src/Pages/OrderPart/OrderPart.js
+++ src/Pages/OrderPart/OrderPart.js
@@ -1,16 +1,30 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 
 const OrderPart = () => {
   const [part, setPart] = useState();
   const { id } = useParams();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
   useEffect(() => {
     (async () => {
       const { data } = await axios.get(`http://localhost:5000/parts/${id}`);
       setPart(data);
     })();
   }, [id]);
+
+  const onSubmit = (data) => {
+    if (data?.quantity > part?.availableQuantity) {
+      return alert("out of stock");
+    }
+    console.log(data);
+  };
 
   return (
     <div className="hero min-h-screen">
@@ -25,7 +39,7 @@ const OrderPart = () => {
           <p className="py-6">{part?.description}</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="card-body">
               <div className="text-center">
                 <h1 className="text-xl text-blue-400 font-bold">
@@ -40,35 +54,127 @@ const OrderPart = () => {
                   type="text"
                   placeholder="Your Name"
                   className="input input-bordered border-primary"
+                  {...register("name", {
+                    required: {
+                      value: true,
+                      message: "Name is Required",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.name?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.name.message}
+                    </span>
+                  )}
+                </label>
               </div>
               <div className="form-control">
                 <input
                   type="email"
                   placeholder="Email"
                   className="input input-bordered border-primary"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is Required",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.email?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.email.message}
+                    </span>
+                  )}
+                </label>
               </div>
               <div className="form-control">
                 <input
                   type="text"
                   placeholder="Address"
                   className="input input-bordered border-primary"
+                  {...register("address", {
+                    required: {
+                      value: true,
+                      message: "Address is Required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Minimum 6 Character",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.address?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.address.message}
+                    </span>
+                  )}
+                  {errors.address?.type === "minLength" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.address.message}
+                    </span>
+                  )}
+                </label>
               </div>
               <div className="form-control">
                 <input
                   type="text"
                   placeholder="Phone Number"
                   className="input input-bordered border-primary"
+                  {...register("phone", {
+                    required: {
+                      value: true,
+                      message: "Number is Required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Minimum 6 Character",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.phone?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.phone.message}
+                    </span>
+                  )}
+                  {errors.phone?.type === "minLength" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.phone.message}
+                    </span>
+                  )}
+                </label>
               </div>
               <div className="form-control">
                 <input
                   type="number"
                   placeholder="Part Quantity"
                   className="input input-bordered border-primary"
+                  {...register("quantity", {
+                    required: {
+                      value: true,
+                      message: "Quantity is Required",
+                    },
+                    min: {
+                      value: 50,
+                      message: "Please Order minimum 50 pieces",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.quantity?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.quantity.message}
+                    </span>
+                  )}
+                  {errors.quantity?.type === "min" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.quantity.message}
+                    </span>
+                  )}
+                </label>
               </div>
 
               <div className="form-control mt-6">
