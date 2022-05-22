1ece76bdd2b721761b7340f14f123abe198c964a
H_A _Opu <wwwhasib4@gmail.com>
2022-05-22 09:50:14 +0600

    install tailwind, react-router and daisyUI,implement simple navbar

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-green-400">Banner</h1>
+    </div>
+  );
+};
+
+export default Banner;
