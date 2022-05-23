fd6d8016a0dc053221877bbe1deb34ed1edc307b
H_A _Opu <wwwhasib4@gmail.com>
2022-05-24 02:15:20 +0600

    showing order details on table

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -6,44 +6,35 @@ const MyOrders = () => {
   const { data: orders } = useQuery("orders", () =>
     fetch("http://localhost:5000/order").then((res) => res.json())
   );
-  // console.log(data);
-  return (
-    <div>
-      <h1 className="text-2xl font-bold text-secondary">
-        My Orders List: {orders.length}
-      </h1>
 
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                Customer name
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <OrderRow key={order._id} order={order} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-10">
+      <table className="w-full text-sm text-left text-gray-500 ">
+        <thead className="text-xs text-gray-300 uppercase bg-gray-700 ">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Customer name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Product name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Quantity
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Payment
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-black ">
+          {orders?.map((order) => (
+            <OrderRow key={order._id} order={order} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/Pages/Dashboard/OrderRow.js
+++ src/Pages/Dashboard/OrderRow.js
@@ -2,25 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const OrderRow = ({ order }) => {
+  const { name, partName, price, quantity } = order;
   return (
     <>
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+      <tr className=" border-b  hover:bg-gray-200 ">
         <th
           scope="row"
-          class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
         >
-          Apple MacBook Pro 17"
+          {name}
         </th>
-        <td class="px-6 py-4">Sliver</td>
-        <td class="px-6 py-4">Laptop</td>
-        <td class="px-6 py-4">$2999</td>
-        <td class="px-6 py-4">$2999</td>
-        <td class="px-6 py-4 text-right">
+        <td className="px-6 py-4">{partName}</td>
+        <td className="px-6 py-4">${price}</td>
+        <td className="px-6 py-4">{quantity}</td>
+
+        <td className="px-6 py-4 text-right">
           <Link
             to="/dashboard"
-            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
           >
-            Edit
+            Paid
           </Link>
         </td>
       </tr>
