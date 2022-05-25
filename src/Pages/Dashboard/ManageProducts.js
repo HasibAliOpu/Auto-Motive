1a5bde8892bc0ad9b83a1d0703ab690f82630506
H_A _Opu <wwwhasib4@gmail.com>
2022-05-25 22:12:38 +0600

    showing all parts on manage products compo

--- src/Pages/Dashboard/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Loading/Loading";
+import ProductCard from "./ProductCard";
 
 const ManageProducts = () => {
+  const {
+    data: parts,
+    isLoading,
+    refetch,
+  } = useQuery("deletePart", () =>
+    fetch("http://localhost:5000/parts").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div>
-      <h1>Manage Products</h1>
+      <h1 className="text-center text-3xl pt-6 text-error">Manage Products</h1>
+      <div className="grid grid-cols-3 gap-5 m-16">
+        {parts.map((part) => (
+          <ProductCard key={part._id} part={part} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Dashboard/ProductCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ProductCard = ({ part }) => {
+  const {
+    _id,
+    img,
+    name,
+    description,
+    price,
+    availableQuantity,
+    minimumQuantity,
+  } = part;
+  return (
+    <div className="card card-compact max-w-md bg-base-100 shadow-xl">
+      <figure>
+        <img src={img} alt="car parts" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+
+        <p className="text-2xl">
+          Price: <span className="text-warning">{price}$</span>
+        </p>
+        <p>{description.slice(0, 250)}...</p>
+        <p className="font-bold">Available Quantity: {availableQuantity}</p>
+        <p className="font-bold">Minimum Quantity: {minimumQuantity}</p>
+        <div className="card-actions justify-center">
+          <button className="btn btn-primary text-white">Purchase</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
