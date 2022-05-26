48226b10fd4001756f5ac8df978001cf58a0b3a5
H_A _Opu <wwwhasib4@gmail.com>
2022-05-26 11:18:41 +0600

    create a function for checking the user is admin or not

---
+++ src/Hooks/useAdmin.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useAdmin = (user) => {
+  const [admin, setAdmin] = useState(false);
+  useEffect(() => {
+    const email = user?.email;
+    if (email) {
+      (async () => {
+        const { data } = await axios.get(
+          `http://localhost:5000/admin/${email}`,
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
+        setAdmin(data.admin);
+      })();
+    }
+  }, [user]);
+  return [admin];
+};
+
+export default useAdmin;
